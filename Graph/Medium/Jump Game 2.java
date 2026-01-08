daa4f3b1152047e209a464859820f9533d5728f9
rv-work <rv.dthought@gmail.com>
2026-01-08 15:43:53 +0530

    JUMP GAME || - tab

--- Graph/Medium/Jump Game 2.java
+++ Graph/Medium/Jump Game 2.java
@@ -50,3 +50,28 @@ class Solution {
         return dp[i] = min;
     }
 }
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int jump = 1; jump <= nums[i] && i + jump < n; jump++) {
+                dp[i + jump] = Math.min(dp[i + jump], dp[i] + 1);
+            }
+        }
+        return dp[n - 1];
+    }
+}
