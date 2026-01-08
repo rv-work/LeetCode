78ae76951a16e088737626ee366a60ef75e139a4
rv-work <rv.dthought@gmail.com>
2026-01-08 15:43:15 +0530

    JUMP GAME || - memo

--- Graph/Medium/Jump Game 2.java
+++ Graph/Medium/Jump Game 2.java
@@ -19,3 +19,34 @@ class Solution {
         return min;
     }
 }
+
+
+
+
+
+
+class Solution {
+    public int jump(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return solve(0, nums, dp);
+    }
+
+    private int solve(int i, int[] nums, int[] dp) {
+        if (i >= nums.length - 1) return 0;
+
+        if (dp[i] != -1) return dp[i];
+
+        int min = Integer.MAX_VALUE;
+
+        for (int jump = 1; jump <= nums[i]; jump++) {
+            int next = i + jump;
+            if (next < nums.length) {
+                int res = solve(next, nums, dp);
+                if (res != Integer.MAX_VALUE)
+                    min = Math.min(min, 1 + res);
+            }
+        }
+        return dp[i] = min;
+    }
+}
