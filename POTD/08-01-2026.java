5e8d5f6da5a59c633c878918b17ee7cd5ef055d1
rv-work <rv.dthought@gmail.com>
2026-01-08 15:24:06 +0530

    POTD : 08-01-2026

---
+++ POTD/08-01-2026.java
@@ -0,0 +1,27 @@
+class Solution {
+    int rec(int i , int j , int[] nums1, int[] nums2){
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+
+        if(i >= n1 || j >= n2) return Integer.MIN_VALUE;
+
+        int mul = nums1[i]*nums2[j] ;
+        int next = rec(i+1, j+1, nums1 , nums2);
+        if(next != Integer.MIN_VALUE){
+            mul = Math.max(mul , mul + next);
+        }
+
+        mul = Math.max(mul , next);
+
+        int notMul = Math.max( rec(i, j+1, nums1 , nums2)  , rec(i+1, j, nums1 , nums2) );
+        
+
+        return Math.max(mul , notMul);
+        
+    }
+
+
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        return rec(0, 0 , nums1 , nums2);
+    }
+}
