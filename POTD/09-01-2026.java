93c5c8391b17b60e8906a799054daa2a51a2cff7
rv-work <rv.dthought@gmail.com>
2026-01-09 13:25:59 +0530

    POTD : 09-01-2026 - BETTER-4

--- POTD/09-01-2026.java
+++ POTD/09-01-2026.java
@@ -255,3 +255,69 @@ class Solution {
         return lastLevel.get(0);
     }
 }
+
+
+
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.offer(root);
+        parent.put(root, null);
+
+        Set<TreeNode> lastLevel = new HashSet<>();
+
+      
+        while (!q.isEmpty()) {
+            int size = q.size();
+            lastLevel.clear();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                lastLevel.add(node);
+
+                if (node.left != null) {
+                    parent.put(node.left, node);
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    parent.put(node.right, node);
+                    q.offer(node.right);
+                }
+            }
+        }
+
+        
+        while (lastLevel.size() > 1) {
+            Set<TreeNode> upper = new HashSet<>();
+            for (TreeNode node : lastLevel) {
+                upper.add(parent.get(node));
+            }
+            lastLevel = upper;
+        }
+
+        return lastLevel.iterator().next();
+    }
+}
