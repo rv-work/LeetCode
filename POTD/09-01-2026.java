80c11a10b668ea4d91b86124d2251989c335e852
rv-work <rv.dthought@gmail.com>
2026-01-09 09:04:31 +0530

    POTD : 09-01-2026 - BETTER

--- POTD/09-01-2026.java
+++ POTD/09-01-2026.java
@@ -55,3 +55,69 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+class Solution {
+
+    TreeNode ans = null;
+
+    int countMatches(TreeNode root, Set<TreeNode> deepestSet) {
+        if (root == null) return 0;
+
+        int count = deepestSet.contains(root) ? 1 : 0;
+
+        count += countMatches(root.left, deepestSet);
+        count += countMatches(root.right, deepestSet);
+
+        return count;
+    }
+
+    void findParent(TreeNode root, Set<TreeNode> deepestSet) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        int need = deepestSet.size();
+
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+
+            if (countMatches(node, deepestSet) == need) {
+                ans = node;
+            }
+
+            if (node.left != null) q.add(node.left);
+            if (node.right != null) q.add(node.right);
+        }
+    }
+
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        if (root == null) return null;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        Set<TreeNode> deepestSet = new HashSet<>();
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            deepestSet.clear();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                deepestSet.add(node);
+
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+        }
+
+        findParent(root, deepestSet);
+        return ans;
+    }
+}
