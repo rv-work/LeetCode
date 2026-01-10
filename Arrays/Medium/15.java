33d418091cba78e254ef6f6811d06c5a0a463bf7
rv-work <rv.dthought@gmail.com>
2026-01-10 17:27:14 +0530

    3 Sum - better

--- Arrays/Medium/15.java
+++ Arrays/Medium/15.java
@@ -28,3 +28,51 @@ class Solution {
         return new ArrayList<>(set);
     }
 }
+
+
+
+
+
+
+
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+
+            // skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    j++;
+                    k--;
+
+                    // skip duplicate j
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+
+                    // skip duplicate k
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return ans;
+    }
+}
