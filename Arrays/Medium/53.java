3cd6e38cb8e4dc013586c0b308b4315922c7c9e3
rv-work <rv.dthought@gmail.com>
2026-01-10 23:26:51 +0530

    53-||

--- Arrays/Medium/53.java
+++ Arrays/Medium/53.java
@@ -46,3 +46,36 @@ class Solution {
         return maxSum;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        Integer[] dp = new Integer[nums.length];
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, solve(nums, i, dp));
+        }
+        return max;
+    }
+
+    int solve(int[] nums, int idx, Integer[] dp) {
+        if (idx == 0)
+            return nums[0];
+
+        if (dp[idx] != null)
+            return dp[idx];
+
+        return dp[idx] =
+            Math.max(nums[idx], nums[idx] + solve(nums, idx - 1, dp));
+    }
+}
