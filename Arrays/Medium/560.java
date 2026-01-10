d48b04a162c6eb4fdc6b75643ba19a50afad4f0e
rv-work <rv.dthought@gmail.com>
2026-01-10 23:09:43 +0530

    560-||

--- Arrays/Medium/560.java
+++ Arrays/Medium/560.java
@@ -18,3 +18,36 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer, Integer> mp = new HashMap<>();
+        int ans = 0;
+
+        int ps = 0;
+        mp.put(ps, 1);  
+
+        for (int i = 0; i < n; i++) {
+            ps += nums[i];
+
+            int need = ps - k;
+
+            ans += mp.getOrDefault(need, 0);
+
+            mp.put(ps, mp.getOrDefault(ps, 0) + 1);
+        }
+
+        return ans;
+    }
+}
