a4997d42eefbf3373b85ccadd52f18e00ebdaa6a
rv-work <rv.dthought@gmail.com>
2026-01-10 23:30:05 +0530

    169-|

--- Arrays/Easy/169.java
+++ Arrays/Easy/169.java
@@ -11,3 +11,25 @@ class Solution {
         return -1;
     }
 }
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        for (int x : nums) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+            if (map.get(x) > n / 2) return x;
+        }
+        return -1;
+    }
+}
