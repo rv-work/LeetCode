e7379497c7179cbb995d08926ea633b77256a74a
rv-work <rv.dthought@gmail.com>
2026-01-10 23:30:53 +0530

    169-|||

--- Arrays/Easy/169.java
+++ Arrays/Easy/169.java
@@ -48,3 +48,24 @@ class Solution {
         return nums[nums.length / 2];
     }
 }
+
+
+
+
+
+
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
