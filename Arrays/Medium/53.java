814fa53b75a27a2226c44adc58613489b1bcb340
rv-work <rv.dthought@gmail.com>
2026-01-10 23:27:24 +0530

    53-|||

--- Arrays/Medium/53.java
+++ Arrays/Medium/53.java
@@ -79,3 +79,52 @@ class Solution {
             Math.max(nums[idx], nums[idx] + solve(nums, idx - 1, dp));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        return divide(nums, 0, nums.length - 1);
+    }
+
+    int divide(int[] nums, int l, int r) {
+        if (l == r)
+            return nums[l];
+
+        int mid = (l + r) / 2;
+
+        int left = divide(nums, l, mid);
+        int right = divide(nums, mid + 1, r);
+        int cross = crossSum(nums, l, mid, r);
+
+        return Math.max(Math.max(left, right), cross);
+    }
+
+    int crossSum(int[] nums, int l, int mid, int r) {
+        int leftSum = Integer.MIN_VALUE, sum = 0;
+        for (int i = mid; i >= l; i--) {
+            sum += nums[i];
+            leftSum = Math.max(leftSum, sum);
+        }
+
+        int rightSum = Integer.MIN_VALUE;
+        sum = 0;
+        for (int i = mid + 1; i <= r; i++) {
+            sum += nums[i];
+            rightSum = Math.max(rightSum, sum);
+        }
+
+        return leftSum + rightSum;
+    }
+}
