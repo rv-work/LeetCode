8061b313cf535cf0813b57a6ff4fe380ea9ee88f
rv-work <rv.dthought@gmail.com>
2026-01-10 21:52:19 +0530

    Reverse Pair - ||

--- Arrays/Hard/409.java
+++ Arrays/Hard/409.java
@@ -55,3 +55,66 @@ class Solution {
 
 
 
+
+class Solution {
+
+    int count = 0;
+
+    void mergeSort(int[] nums, int l, int r) {
+        if (l >= r)
+            return;
+        int mid = l + (r - l) / 2;
+        mergeSort(nums, l, mid);
+        mergeSort(nums, mid + 1, r);
+        merge(nums, l, mid, r);
+    }
+
+    private void merge(int[] nums, int l, int mid, int r) {
+
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            left[i] = nums[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            right[j] = nums[mid + 1 + j];
+        }
+        // ans calculate krenge phle fir merge ....
+        int j = 0;
+        for (int i = 0; i < n1; i++) {
+            while (j < n2 && (long) left[i] > 2L * right[j]) {
+                j++;
+            }
+            count += j;
+        }
+
+        int i = 0;
+        j = 0;
+        int k = l;
+
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                nums[k++] = left[i++];
+            } else {
+                nums[k++] = right[j++];
+            }
+        }
+
+        while (i < n1) {
+            nums[k++] = left[i++];
+        }
+
+        while (j < n2) {
+            nums[k++] = right[j++];
+        }
+    }
+
+    public int reversePairs(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return count;
+    }
+}
