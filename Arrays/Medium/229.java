bf4f2eec28e710e2d1f8cd5d931e4accd3078a68
rv-work <rv.dthought@gmail.com>
2026-01-10 17:02:28 +0530

    Majority Element || -sort

--- Arrays/Medium/229.java
+++ Arrays/Medium/229.java
@@ -71,3 +71,46 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        List<Integer> ans = new ArrayList<>();
+
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                if (count > n / 3) {
+                    ans.add(nums[i - 1]);
+                }
+                count = 1;
+            }
+        }
+
+
+        if (count > n / 3) {
+            ans.add(nums[n - 1]);
+        }
+
+        return ans;
+    }
+}
