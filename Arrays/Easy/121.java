36cfe6d714b7cac782db8abcae475dfb355c35de
rv-work <rv.dthought@gmail.com>
2026-01-10 23:23:40 +0530

    121 - |

--- Arrays/Easy/121.java
+++ Arrays/Easy/121.java
@@ -11,3 +11,50 @@ class Solution {
         return max;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+
+    public int maxProfit(int[] prices) {
+        return solve(prices, 0, false, false);
+    }
+
+    int solve(int[] prices, int idx, boolean bought, boolean sold) {
+
+        // base case
+        if (idx == prices.length || sold)
+            return 0;
+
+        int profit = 0;
+
+        // option: skip
+        profit = solve(prices, idx + 1, bought, sold);
+
+        // option: buy (only if not bought yet)
+        if (!bought) {
+            profit = Math.max(
+                profit,
+                -prices[idx] + solve(prices, idx + 1, true, sold)
+            );
+        }
+
+        // option: sell (only if bought and not sold)
+        if (bought && !sold) {
+            profit = Math.max(
+                profit,
+                prices[idx] + solve(prices, idx + 1, false, true)
+            );
+        }
+
+        return profit;
+    }
+}
