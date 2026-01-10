a47647b429bd66a75abd803bbc27dc6614c3c3ab
rv-work <rv.dthought@gmail.com>
2026-01-10 23:32:34 +0530

    75-||

--- Arrays/Medium/75.java
+++ Arrays/Medium/75.java
@@ -33,3 +33,27 @@ class Solution {
         Arrays.sort(nums);
     }
 }
+
+
+
+
+
+
+
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int c0 = 0, c1 = 0, c2 = 0;
+
+        for (int x : nums) {
+            if (x == 0) c0++;
+            else if (x == 1) c1++;
+            else c2++;
+        }
+
+        int i = 0;
+        while (c0-- > 0) nums[i++] = 0;
+        while (c1-- > 0) nums[i++] = 1;
+        while (c2-- > 0) nums[i++] = 2;
+    }
+}
