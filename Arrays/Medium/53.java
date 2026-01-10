40a1699a391485054d20f1631cfdda92c1496ed6
rv-work <rv.dthought@gmail.com>
2026-01-10 23:26:35 +0530

    53-|

--- Arrays/Medium/53.java
+++ Arrays/Medium/53.java
@@ -16,3 +16,33 @@ class Solution {
     return maxSum;
     }
 }
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int sum = prefix[j + 1] - prefix[i];
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+        return maxSum;
+    }
+}
