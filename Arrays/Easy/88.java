2d75cd6b35ef92c6736dd0fb7fd739a95bfe64c0
rv-work <rv.dthought@gmail.com>
2026-01-10 20:09:23 +0530

    Merge Sorted Array - ||

--- Arrays/Easy/88.java
+++ Arrays/Easy/88.java
@@ -7,3 +7,35 @@ class Solution {
         Arrays.sort(nums1);
     }
 }
+
+
+
+
+
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int[] arr = new int[m+n];
+
+        int i = 0, j = 0, k = 0;
+
+       
+        while (i<m && j<n) {
+          if(nums1[i] > nums2[j]){
+            arr[k++] =  nums2[j++];
+          }else {
+            arr[k++] =  nums1[i++];
+          }
+        }
+
+
+        while(i<m)arr[k++] = nums1[i++];
+        while(j<n)arr[k++] = nums2[j++];
+
+      
+        for (int x = 0; x < m+n; x++) {
+            nums1[x] = arr[x];
+        }
+    }
+}
