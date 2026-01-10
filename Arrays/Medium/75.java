88e3a76834f7c0fd91d7082bc310b7d2c635b34f
rv-work <rv.dthought@gmail.com>
2026-01-10 23:31:31 +0530

    75

---
+++ Arrays/Medium/75.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left = 0, mid = 0, right = nums.length - 1;
+        while (mid <= right) {
+            if (nums[mid] == 1)
+                mid++;
+            else if (nums[mid] == 0) {
+                int temp = nums[mid];
+                nums[mid] = nums[left];
+                nums[left] = temp;
+                left++;
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[right];
+                nums[right] = temp;
+                right--;
+            }
+        }
+    }
+}
