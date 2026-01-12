9c9aa550c793dee823ee8e34b775935079d6d295
rv-work <rv.dthought@gmail.com>
2026-01-12 18:09:37 +0530

    POTD : 12-01-2026

--- POTD/12-01-2026.java
+++ POTD/12-01-2026.java
@@ -1,14 +1,31 @@
 class Solution {
-    public int minTimeToVisitAllPoints(int[][] points) {
-        int n = points.length;
-        int ans = 0;
-        for(int i = 0; i<n-1; i++){
-            int x = Math.abs(points[i+1][0] -  points[i][0]);
-            int y = Math.abs(points[i+1][1] -  points[i][1]);
-            int min = Math.min(x , y);
-            ans += x + y - min; // ans += min , ans += x-min , ans += y-min
-        }
-
-        return ans;
+  public int minTimeToVisitAllPoints(int[][] points) {
+    int n = points.length;
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++) {
+      int x = Math.abs(points[i + 1][0] - points[i][0]);
+      int y = Math.abs(points[i + 1][1] - points[i][1]);
+      int min = Math.min(x, y);
+      ans += x + y - min; // ans += min , ans += x-min , ans += y-min
     }
+
+    return ans;
+  }
 }
+
+class Solution {
+  public int minTimeToVisitAllPoints(int[][] p) {
+    int ans = 0;
+    for (int i = 1; i < p.length; i++) {
+      ans += Math.max(
+          Math.abs(p[i][0] - p[i - 1][0]),
+          Math.abs(p[i][1] - p[i - 1][1])
+
+      );
+    }
+    return ans;
+  }
+}
+
+// (p[i,0] - p[i-1][0]) x or pre ke x ka difference
+// (p[i,1] - p[i-1][1]) y or pre ke y ka diff
