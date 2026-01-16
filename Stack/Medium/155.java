42f6ecfa09dac492144911fb171347cd45a12b42
rv-work <rv.dthought@gmail.com>
2026-01-16 13:20:34 +0530

    155

---
+++ Stack/Medium/155.java
@@ -0,0 +1,53 @@
+class MinStack {
+    Stack<Integer> st;
+    int minIndex = -1;
+    int idx = -1;
+    int minValue = Integer.MAX_VALUE;
+    Map<Integer , Integer> map ;
+
+    public MinStack() {
+        st = new Stack<>();
+        map = new HashMap<>();
+    }
+    
+    public void push(int val) {
+        idx++;
+        map.put(idx , val);
+
+        if (val <= minValue) {
+            minValue = val;
+            minIndex = idx;
+        }
+
+        st.push(minIndex);
+    }
+    
+    public void pop() {
+        int removedIndex = idx;
+        map.remove(idx);
+        st.pop();
+        idx--;
+
+        // 🔥 FIX: if minimum element was popped
+        if (removedIndex == minIndex) {
+            minValue = Integer.MAX_VALUE;
+            minIndex = -1;
+
+            for (int key : map.keySet()) {
+                int v = map.get(key);
+                if (v <= minValue) {
+                    minValue = v;
+                    minIndex = key;
+                }
+            }
+        }
+    }
+    
+    public int top() {
+        return map.get(idx);
+    }
+    
+    public int getMin() {
+        return map.get(st.peek());
+    }
+}
