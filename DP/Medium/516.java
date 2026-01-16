8cfc42010cd7d1713a27b45bf5eaeaad8023dee6
rv-work <rv.dthought@gmail.com>
2026-01-16 14:02:09 +0530

    516

--- DP/Medium/516.java
+++ DP/Medium/516.java
@@ -17,3 +17,33 @@ class Solution {
         return res(s, 0, s.length() - 1);
     }
 }
+
+
+
+
+
+
+class Solution {
+    int res(String s, int i, int j , int[][] dp ) {
+
+        if (i > j) return 0;
+        if (i == j) return 1;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if (s.charAt(i) == s.charAt(j))
+            return dp[i][j] =  2 + res(s, i + 1, j - 1 , dp);
+
+        return dp[i][j] =  Math.max(
+            res(s, i + 1, j , dp),
+            res(s, i, j - 1 , dp)
+        );
+    }
+
+    public int longestPalindromeSubseq(String s) {
+        int  n = s.length();
+        int[][] dp  = new int[n][n];
+        for(int arr [] : dp) Arrays.fill(arr , -1);
+        return res(s, 0, s.length() - 1 , dp);
+    }
+}
