6c399301d6b2f43caa7ff95e0f5b9552b925d57e
rv-work <rv.dthought@gmail.com>
2026-01-16 17:44:21 +0530

    57

---
+++ Stack/Medium/57.java
@@ -0,0 +1,68 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n = intervals.length;
+
+        // special case
+        if (n == 0) {
+            return new int[][]{ newInterval };
+        }
+
+        int st = n;
+        int end = n;
+
+        // find st
+        for (int i = 0; i < n; i++) {
+            if (intervals[i][1] >= newInterval[0]) {
+                st = i;
+                break;
+            }
+        }
+
+        // find end
+        for (int i = st; i < n; i++) {
+            if (intervals[i][0] > newInterval[1]) {
+                end = i - 1;
+                break;
+            }
+        }
+
+        if (st == n) {
+            // newInterval comes after all
+            int[][] ans = new int[n + 1][2];
+            for (int i = 0; i < n; i++) ans[i] = intervals[i];
+            ans[n] = newInterval;
+            return ans;
+        }
+
+        if (end == n) end = n - 1;
+
+        // merge interval
+        int[][] merged = new int[1][2];
+        merged[0][0] = newInterval[0];
+        merged[0][1] = newInterval[1];
+
+        for (int i = st; i <= end; i++) {
+            merged[0][0] = Math.min(merged[0][0], intervals[i][0]);
+            merged[0][1] = Math.max(merged[0][1], intervals[i][1]);
+        }
+
+        // build answer
+        int[][] ans = new int[n - (end - st)][2];
+        int idx = 0;
+
+        // before st
+        for (int i = 0; i < st; i++) {
+            ans[idx++] = intervals[i];
+        }
+
+        // merged interval
+        ans[idx++] = merged[0];
+
+        // after end
+        for (int i = end + 1; i < n; i++) {
+            ans[idx++] = intervals[i];
+        }
+
+        return ans;
+    }
+}
