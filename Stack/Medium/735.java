cae3f1adcd2f78df7c083687db399a9abd577e58
rv-work <rv.dthought@gmail.com>
2026-01-16 18:33:57 +0530

    735

--- Stack/Medium/735.java
+++ Stack/Medium/735.java
@@ -72,3 +72,45 @@ class Solution {
         return res;
     }
 }
+
+
+
+
+
+
+
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int a : asteroids) {
+            boolean alive = true;
+
+            while (!list.isEmpty() && list.get(list.size()-1) > 0 && a < 0) {
+                int top = list.get(list.size()-1);
+
+                if (top < -a) {
+                    list.remove(list.size()-1);
+                } else if (top == -a) {
+                    list.remove(list.size()-1);
+                    alive = false;
+                    break;
+                } else {
+                    alive = false;
+                    break;
+                }
+            }
+
+            if (alive) list.add(a);
+        }
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
+
+
+// int[] arr = new int[list.size()];
+// for (int i = 0; i < list.size(); i++) {
+//     arr[i] = list.get(i);
+// }
