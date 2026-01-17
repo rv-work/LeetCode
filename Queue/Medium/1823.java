bd01ffd3209d1c2562899ed565fab6fe72839657
rv-work <rv.dthought@gmail.com>
2026-01-17 15:48:11 +0530

    1823

--- Queue/Medium/1823.java
+++ Queue/Medium/1823.java
@@ -64,3 +64,44 @@ class Solution {
         return ansIndex(n , k) + 1;
     }
 }
+
+
+
+class Solution {
+
+    int ansIndex(int n , int k){
+        if(n==1) return 0;
+
+        int idx = ansIndex(n-1 , k);
+        idx += k; 
+        
+// to get index in original array... kyunki hm hr bari +k index se start krte
+// kyunki see , n = 5 m k = 2
+//    ind = 0,1,2,3,4 
+//         [1,2,3,4,5]
+// so ... after 1st .. we will delete 2 and start again from 3 so phle vale pichhhe aa jayenge
+//      idx:0,1,2,3.... 
+     //    [3,4,5,1]
+
+// so for 3.. to get original indx .... curr  + k .. kyunki hm hr step me k minuse kr dete hain use aage lane ke liye isiliye... phle 2 the pichhe lane ke liye k minuse kiya so balance krne ke liye +k
+   
+
+
+
+
+
+        idx %= n; // to make circular move
+
+        return idx;
+    }
+    public int findTheWinner(int n, int k) {
+        return ansIndex(n , k) + 1;
+    }
+}
+
+
+
+
+
+
+
