e2bde2bec4ca87f765fb88c335e68ac5c2a16a70
rv-work <rv.dthought@gmail.com>
2026-01-17 11:02:05 +0530

    22

---
+++ Queue/Easy/222.java
@@ -0,0 +1,42 @@
+class MyStack {
+    Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q.add(x);
+    }
+    
+    public int pop() {
+        int size = q.size();
+        for(int i = 1; i<= size-1; i++){
+            q.add(q.poll());
+        }
+        return q.poll();
+    }
+    
+    public int top() {
+         int size = q.size();
+        for(int i = 1; i<= size-1; i++){
+            q.add(q.poll());
+        }
+        int top =  q.peek();
+        q.offer(q.poll());
+        return top;
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
