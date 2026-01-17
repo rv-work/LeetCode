bb89b1ab02fa1aa16b0001db674c3c669d8d5435
rv-work <rv.dthought@gmail.com>
2026-01-17 12:40:18 +0530

    239

--- Queue/Hard/239.java
+++ Queue/Hard/239.java
@@ -138,3 +138,54 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+
+            
+            if (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+
+            
+            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
+                dq.pollLast();
+            }
+
+            dq.addLast(i);
+
+           
+            if (i >= k - 1) {
+                ans[i - k + 1] = nums[dq.peekFirst()];
+            }
+        }
+
+        return ans;
+    }
+}
+
+
+// idea is to maintain the Decreasing order ...in Deque
