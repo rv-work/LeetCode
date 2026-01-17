8bce8fd6940ad188cae05236641f6d3eb623dc6f
rv-work <rv.dthought@gmail.com>
2026-01-17 11:45:18 +0530

    622

--- Queue/Medium/622.java
+++ Queue/Medium/622.java
@@ -33,6 +33,7 @@ class MyCircularQueue {
         if(r==-1) return -1;
         return arr[r];
     }
+
     
     public boolean isEmpty() {
         return r == -1;
@@ -40,6 +41,84 @@ class MyCircularQueue {
     
     public boolean isFull() {
         return r == arr.length-1;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
+
+
+
+
+
+
+
+
+
+class MyCircularQueue {
+
+    int [] arr;
+    int r;
+    int f;
+    int size ;
+
+    public MyCircularQueue(int k) {
+        arr = new int[k];
+        r = -1;
+        f = -1;
+        size = k;
+    }
+    
+    public boolean enQueue(int value) {
+        if(isFull()) return false;
+        if(isEmpty()) 
+        {
+            f = 0;
+            r = 0;
+        }
+        else {
+            r = (r+1)%size;
+        }
+        arr[r] = value;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if(isEmpty()) return false;
+        
+        if(f == r ) {
+            f =-1;
+            r=-1;
+        }
+        else f = (f + 1)%size;
+        
+        return true;
+    }
+    
+    public int Front() {
+        if(isEmpty()) return -1;
+        return arr[f];
+    }
+    
+    public int Rear() {
+        if(isEmpty()) return -1;
+        return arr[r];
+    }
+    
+    public boolean isEmpty() {
+        return r == -1 && f == -1;
+    }
+    
+    public boolean isFull() {
+        return (r+1) % size == f;
     }
 }
 
