8de0a40d05369437b2023d20a8eaba0c4cd77686
rv-work <rv.dthought@gmail.com>
2026-01-18 18:02:16 +0530

    34

--- Arrays/Medium/34.java
+++ Arrays/Medium/34.java
@@ -38,3 +38,48 @@ class Solution {
         return new int[]{lb, ub};
     }
 }
+
+
+
+
+
+
+
+
+
+class Solution {
+
+    int lowerBound(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] >= target)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+
+    int upperBound(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r + 1) / 2;
+            if (nums[mid] <= target)
+                l = mid;
+            else
+                r = mid - 1;
+        }
+        return l;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) return new int[]{-1, -1};
+
+        int lb = lowerBound(nums, target);
+        if (nums[lb] != target) return new int[]{-1, -1};
+
+        int ub = upperBound(nums, target);
+        return new int[]{lb, ub};
+    }
+}
