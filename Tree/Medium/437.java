078f12bae11427eb9638592967fbc1cf8cf18723
rv-work <rv.dthought@gmail.com>
2026-01-18 22:17:25 +0530

    437

--- Tree/Medium/437.java
+++ Tree/Medium/437.java
@@ -23,3 +23,54 @@ class Solution {
         return count;
     }
 }
+
+
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    public int pathSum(TreeNode root, int targetSum) {
+        Map<Long, Integer> map = new HashMap<>();
+        map.put(0L, 1); // base prefix
+
+        return dfs(root, 0L, targetSum, map);
+    }
+
+    int dfs(TreeNode root, long currSum, int target, Map<Long, Integer> map) {
+        if (root == null) return 0;
+
+        currSum += root.val;
+
+        int count = map.getOrDefault(currSum - target, 0);
+
+        map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+
+        count += dfs(root.left, currSum, target, map);
+        count += dfs(root.right, currSum, target, map);
+
+        // backtrack
+        map.put(currSum, map.get(currSum) - 1);
+
+        return count;
+    }
+}
