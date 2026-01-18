26d62f5fb2aa65299e20e2fcdc6787af2f369831
rv-work <rv.dthought@gmail.com>
2026-01-18 19:20:31 +0530

    590

--- Tree/Easy/590.java
+++ Tree/Easy/590.java
@@ -81,3 +81,47 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Stack<Node> s = new Stack<>();
+        s.push(root);
+
+        while(!s.isEmpty()){
+            Node node = s.pop();
+
+            for(Node child : node.children){
+                if(child != null) s.push(child);
+            }
+
+            ans.addFirst(node.val);
+        }
+        
+        return ans;
+    }
+}
