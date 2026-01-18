d24b82ce40866cd135a34b45cff8965936b392a2
rv-work <rv.dthought@gmail.com>
2026-01-18 23:15:10 +0530

    63

--- DP/Medium/63.java
+++ DP/Medium/63.java
@@ -72,3 +72,44 @@ class Solution {
         return dp[0][0];
     }
 }
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int [] next = new int[n+1];
+        int [] curr = new int[n+1];
+
+        if (obstacleGrid[m - 1][n - 1] == 1) return 0;
+        curr[n - 1] = 1;
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (obstacleGrid[i][j] == 1) {
+                    curr[j] = 0;
+                    continue;
+                }
+
+                if (i == m - 1 && j == n - 1) continue;
+
+                int down =  next[j] ;
+                int right = curr[j + 1] ;
+
+                curr[j] = down + right;
+            }
+            next = curr;
+        }
+
+        return curr[0];
+    }
+}
