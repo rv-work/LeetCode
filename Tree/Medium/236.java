f13ead43a66ff7e41ade5ea0fd83ee12b5c80150
rv-work <rv.dthought@gmail.com>
2026-01-18 18:47:54 +0530

    236

---
+++ Tree/Medium/236.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+         if(root == null) return null;
+
+         TreeNode left = lowestCommonAncestor(root.left , p , q);
+         TreeNode right = lowestCommonAncestor(root.right , p , q);
+
+        if((left == p && right == q) || (left == q && right == p)) return root;
+        if(left == null && right == null) {
+            if(root == p || root == q) return root;
+            return null;
+        }
+
+        if(left == null ){
+           if(right == p && root == q || right == q && root == p) return root;
+           return right;
+        } else {
+            if(left == p && root == q || left == q && root == p) return root;
+           return left;
+        }
+    }
+}
