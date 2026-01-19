520abdc2e476caacec81dbc213c71ac1f5cdf9c1
rv-work <rv.dthought@gmail.com>
2026-01-19 16:52:02 +0530

    2192

--- Graph/Medium/2192.java
+++ Graph/Medium/2192.java
@@ -43,3 +43,52 @@ class Solution {
         return res;
     }
 }
+
+
+
+
+
+
+
+
+class Solution {
+
+    List<List<Integer>> rev;
+    List<Set<Integer>> ans;
+
+    public List<List<Integer>> getAncestors(int n, int[][] edges) {
+
+        rev = new ArrayList<>();
+        ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            rev.add(new ArrayList<>());
+            ans.add(new TreeSet<>());
+        }
+
+        // reverse graph
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            rev.get(v).add(u);
+        }
+
+        // DFS for every node
+        for (int i = 0; i < n; i++) {
+            dfs(i, i);
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            res.add(new ArrayList<>(ans.get(i)));
+        }
+        return res;
+    }
+
+    void dfs(int start, int node) {
+        for (int parent : rev.get(node)) {
+            if (ans.get(start).add(parent)) {
+                dfs(start, parent);
+            }
+        }
+    }
+}
