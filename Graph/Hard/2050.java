0e41e4741e74b0cb41596bd06b9486601b9293e7
rv-work <rv.dthought@gmail.com>
2026-01-20 13:19:36 +0530

    2050

--- Graph/Hard/2050.java
+++ Graph/Hard/2050.java
@@ -112,3 +112,75 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int minimumTime(int n, int[][] relations, int[] time) {
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+
+        int[] indeg = new int[n];
+        for (int[] r : relations) {
+            int u = r[0] - 1, v = r[1] - 1;
+            adj.get(u).add(v);
+            indeg[v]++;
+        }
+
+        int[] finish = new int[n];
+        int[] best = new int[n];  // max prereq finish
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            finish[i] = time[i];
+            if (indeg[i] == 0) q.add(i);
+        }
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+
+            for (int v : adj.get(u)) {
+                best[v] = Math.max(best[v], finish[u]);
+                indeg[v]--;
+
+                if (indeg[v] == 0) {
+                    finish[v] += best[v];
+                    q.add(v);
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int x : finish) ans = Math.max(ans, x);
+        return ans;
+    }
+}
+
+
+// int the previous version /...
+
+//  int max = 0;
+//  for (int dep : adj2.get(nei)) {
+//      max = Math.max(max, finish[dep]);
+//  }
+//  finish[nei] += max;
+
+// hm dekhte the ki jab ye taiuar hai hone ke liye ab dekho iske pre me se kiska max time tha .. usi me iske time ko add krlo//...
+
+
+// but usko hm phle se hi calculate kr skte hain ki ...
+// sbse phle best initilise krlo ki sab 0 hain
+// ab jis point or mai khatam ho gya to to mere jo bhi nei hain un pr bta do ki unko start hone ke liye mere khatam hone tak ka time chahiye hoga vo update krdo... now ho skta hai same node 1 se jyada pr depene ho... to vo vha store hone ke baad jab dusra ayega to bolega mai bhi ho gya update kr do to ab agar ye phle vale se jyada le rha tha to ye aayega otherwise vo already isse jyada wait krne hi vala hai to ye affect nhi krega ... thats why here .....
+
+//  best[v] = Math.max(best[v], finish[u]);
+
