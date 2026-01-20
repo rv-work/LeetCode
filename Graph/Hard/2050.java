30aa34411061fb107e521928cd505d0924d7b220
rv-work <rv.dthought@gmail.com>
2026-01-20 12:49:58 +0530

    2050

--- Graph/Hard/2050.java
+++ Graph/Hard/2050.java
@@ -37,3 +37,78 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+// this will not work ..because .. any course can start as early possible... not level wise
+// means if C1(2) ,C2(3) , C3(5).... running .......next C4(3) aur C4 ke prerequisites C1 & C2 hain to ye unke end hote hi chalne lg jayega naki C3 ka wait krega/..
+
+// so total time .... 6.. not 8 ( upper)...
+
+// shi vala niche hai... see////////////////
+
+
+
+
+class Solution {
+    public int minimumTime(int n, int[][] relations, int[] time) {
+
+        List<List<Integer>> adj = new ArrayList<>();
+        List<List<Integer>> adj2 = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+            adj2.add(new ArrayList<>());
+        }
+
+        int[] in = new int[n];
+
+        for (int[] rel : relations) {
+            int prev = rel[0] - 1;
+            int next = rel[1] - 1;
+
+            adj.get(prev).add(next);
+            adj2.get(next).add(prev);
+
+            in[next]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        int[] finish = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            finish[i] = time[i];
+            if (in[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+
+            for (int nei : adj.get(node)) {
+
+                in[nei]--;
+
+                if (in[nei] == 0) {
+                    // compute max finish of all prerequisites
+                    int max = 0;
+                    for (int dep : adj2.get(nei)) {
+                        max = Math.max(max, finish[dep]);
+                    }
+                    finish[nei] += max;
+
+                    q.add(nei);
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int f : finish) ans = Math.max(ans, f);
+
+        return ans;
+    }
+}
