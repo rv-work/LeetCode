4e036fed1cc4630a5630efe0582d30491f4cd9a7
rv-work <rv.dthought@gmail.com>
2026-01-21 15:40:59 +0530

    154

---
+++ Arrays/Medium/154.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+
+        int l = 0; int r = nums.length-1;
+
+        while(l < r){
+            int mid = (l+r)/2;
+            if(nums[mid] < nums[r]) r = mid;
+            else if(nums[mid] > nums[r])  l = mid+1;
+            else r--;
+        }
+
+        return nums[l];
+    }
+}
