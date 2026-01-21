32a482bbcc1f440aabbce194dac5c0cfde316da8
rv-work <rv.dthought@gmail.com>
2026-01-21 19:46:02 +0530

    69

--- Arrays/Easy/69.java
+++ Arrays/Easy/69.java
@@ -20,3 +20,35 @@ class Solution {
         return right;        
     }
 }
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int count = 0;
+        int temp = x;
+        while (temp > 0) {
+            count++;
+            temp >>= 1;
+        }
+
+        int result = 0;
+        for (int i = (count + 1) / 2; i >= 0; i--) {
+            result |= (1 << i);
+            if ((long) result * result > x) {
+                result ^= (1 << i);
+            }
+        }
+
+        return result;        
+    }
+}
