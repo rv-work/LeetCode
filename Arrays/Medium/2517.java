4c26f318a9b5c880d623e66c6d7644f32df7bdca
rv-work <rv.dthought@gmail.com>
2026-01-22 19:59:59 +0530

    2517

--- Arrays/Medium/2517.java
+++ Arrays/Medium/2517.java
@@ -37,3 +37,53 @@ class Solution {
         
     }
 }
+
+
+
+
+
+
+
+
+
+class Solution {
+    int check(int [] price , int gap){
+        int count = 1; int target = price[0] + gap;
+        for(int i = 1; i< price.length; i++){
+            if(price[i] >= target){
+                count++;
+                target = price[i] + gap;
+            }
+        }
+
+
+        return count;
+
+    }
+    public int maximumTastiness(int[] price, int k) {
+        Arrays.sort(price);
+        int  n = price.length;
+
+        int l = Integer.MAX_VALUE;
+        int r = price[n-1] - price[0];
+
+        for(int i = 0; i<n-1; i++){
+          l = Math.min(l , price[i+1]-price[i]);
+        }
+
+        int ans = -1;
+
+        while(l <= r){
+            int mid = (l+r)/2;
+            int possible = check(price , mid);
+            if(possible >= k){
+                ans = mid;
+                l = mid+1;
+            } 
+            else r = mid-1;
+        }
+
+        return ans;
+        
+    }
+}
