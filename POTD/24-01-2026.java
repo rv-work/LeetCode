f2a6414df851e20eef12c2a89959e3696abf57ad
rv-work <rv.dthought@gmail.com>
2026-01-24 08:12:59 +0530

    POTD : 24-01-2026

---
+++ POTD/24-01-2026.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0; i<n/2; i++){
+            ans = Math.max(ans , nums[i]+nums[n-1-i]);
+        }
+
+        return ans;
+    }
+}
