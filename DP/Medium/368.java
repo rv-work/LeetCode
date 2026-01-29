299b31474015f2b41a52ddd816efabfe19a3abc3
rv-work <rv.dthought@gmail.com>
2026-01-29 19:26:23 +0530

    368

--- DP/Medium/368.java
+++ DP/Medium/368.java
@@ -73,3 +73,52 @@ class Solution {
         return dp[0][0];
     }
 }
+
+
+
+
+
+
+
+class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums);
+        int n =  nums.length;
+        int dp[] = new int[n];
+        int prev[] = new int[n];
+
+        Arrays.fill(dp , 1);
+        Arrays.fill(prev , -1);
+
+        int maxLen = 0; int index = -1;
+
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<i; j++){
+                if(nums[i] % nums[j] == 0 && dp[j]+1 > dp[i]){
+                    dp[i] = dp[j]+1;
+                    prev[i] = j;
+
+                    // if(maxLen < dp[i]){
+                    //     maxLen = Math.max(maxLen , dp[i]);
+                    //     index = i;
+                    // } .. ye galat ho jayega .. kyunki hme best chahiye .. pure j = 0 to i-1 tak me 
+                   
+                }
+            }
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                index = i;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        while(index > -1){
+            ans.add(nums[index]);
+            index = prev[index];
+        }
+
+        return ans;
+    }
+}
