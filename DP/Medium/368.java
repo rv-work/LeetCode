67cbafb269e3e2aa56dd95fdeff6d8d4072f7812
rv-work <rv.dthought@gmail.com>
2026-01-29 18:55:28 +0530

    368

--- DP/Medium/368.java
+++ DP/Medium/368.java
@@ -23,3 +23,53 @@ class Solution {
         return res(0,-1,nums,n , dp);
     }
 }
+
+
+
+
+
+
+
+
+class Solution {
+
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        // dp[idx][last+1] = best subset from idx when last chosen = last
+        List<Integer>[][] dp = new List[n+1][n+1];
+
+        // ---- BASE CASE: idx == n ----
+        // From idx = n, answer is empty list for ANY last
+        for (int last = -1; last < n; last++) {
+            dp[n][last + 1] = new ArrayList<>();
+        }
+
+        // ---- FILL TABLE bottom-up ----
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int last = idx - 1; last >= -1; last--) {
+
+                // -------- NOT TAKE --------
+                List<Integer> notTake = dp[idx + 1][last + 1];
+
+                // -------- TAKE --------
+                List<Integer> take = new ArrayList<>();
+                if (last == -1 || nums[idx] % nums[last] == 0) {
+                    take.add(nums[idx]);
+                    take.addAll(dp[idx + 1][idx + 1]);
+                }
+
+                // -------- BEST --------
+                if (take.size() > notTake.size()) {
+                    dp[idx][last + 1] = take;
+                } else {
+                    dp[idx][last + 1] = notTake;
+                }
+            }
+        }
+
+        // answer = dp[0][-1+1] = dp[0][0]
+        return dp[0][0];
+    }
+}
