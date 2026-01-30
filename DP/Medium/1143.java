8f30e130988bf83a5b4cc8b64c722f77bf808647
rv-work <rv.dthought@gmail.com>
2026-01-30 17:47:05 +0530

    1143

--- DP/Medium/1143.java
+++ DP/Medium/1143.java
@@ -25,3 +25,32 @@ class Solution {
         return res(0,0,text1,text2,dp);
     }
 }
+
+
+
+
+
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n1 = text1.length();
+        int n2 = text2.length();
+        int dp[][] = new int[n1 + 1][n2 + 1];
+
+        for (int i = n1 - 1; i >= 0; i--) {
+            for (int j = n2 - 1; j >= 0; j--) {
+
+                int match = Integer.MIN_VALUE;
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    match = 1 + dp[i + 1][j + 1];
+                }
+                int moveI = dp[i + 1][j];
+                int moveJ = dp[i][j + 1];
+
+                dp[i][j] = Math.max(match, Math.max(moveI, moveJ));
+            }
+        }
+
+        return dp[0][0];
+    }
+}
