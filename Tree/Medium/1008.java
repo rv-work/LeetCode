edc58512dd87d6d9532f2152a53c9f7d7defbd57
rv-work <rv.dthought@gmail.com>
2026-01-30 19:54:47 +0530

    1008

--- Tree/Medium/1008.java
+++ Tree/Medium/1008.java
@@ -38,3 +38,66 @@ class Solution {
         return root;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private void insert(TreeNode root, int val) {
+        TreeNode curr = root;
+
+        while (true) {
+            if (val < curr.val) {
+                // go left
+                if (curr.left == null) {
+                    curr.left = new TreeNode(val);
+                    break;
+                } else {
+                    curr = curr.left;
+                }
+            } else {
+                // go right
+                if (curr.right == null) {
+                    curr.right = new TreeNode(val);
+                    break;
+                } else {
+                    curr = curr.right;
+                }
+            }
+        }
+    }
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+
+        TreeNode root = new TreeNode(preorder[0]);
+
+        for (int i = 1; i < preorder.length; i++) {
+            insert(root, preorder[i]);
+        }
+
+        return root;
+    }
+}
