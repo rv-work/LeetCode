685f944cca2c78a58ebf5e5ce76c0d8250bbd7f1
rv-work <rv.dthought@gmail.com>
2026-01-31 13:35:53 +0530

    1334

--- Graph/Medium/1334.java
+++ Graph/Medium/1334.java
@@ -49,3 +49,69 @@ class Solution {
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+
+        List<int[]>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+
+        for (int[] e : edges) {
+            graph[e[0]].add(new int[]{e[1], e[2]});
+            graph[e[1]].add(new int[]{e[0], e[2]});
+        }
+
+        int bestCity = -1;
+        int bestCount = Integer.MAX_VALUE;
+
+        for (int start = 0; start < n; start++) {
+            int count = dijkstra(start, graph, n, distanceThreshold);
+
+            if (count <= bestCount) {
+                bestCount = count;
+                bestCity = start;   
+            }
+        }
+
+        return bestCity;
+    }
+
+    private int dijkstra(int src, List<int[]>[] graph, int n, int limit) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        pq.add(new int[]{src, 0});
+        int count = 0;
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int u = cur[0], d = cur[1];
+
+            if (d > dist[u]) continue;
+            if (d > limit) continue; // early stop
+
+            count++;
+
+            for (int[] nei : graph[u]) {
+                int v = nei[0], w = nei[1];
+                if (d + w < dist[v] && d + w <= limit) {
+                    dist[v] = d + w;
+                    pq.add(new int[]{v, dist[v]});
+                }
+            }
+        }
+
+        return count;
+    }
+}
