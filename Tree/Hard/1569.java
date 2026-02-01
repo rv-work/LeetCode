9ef382aff93dd4fa295eebe71ef303c3ec9e59f8
rv-work <rv.dthought@gmail.com>
2026-02-01 17:59:34 +0530

    1569

--- Tree/Hard/1569.java
+++ Tree/Hard/1569.java
@@ -63,3 +63,63 @@ class Solution {
         return (ans - 1) % 1_000_000_007;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    long MOD = 1000000007;
+
+    private long solve(List<Integer> arr, long[][] nCr) {
+        int n = arr.size();
+        if (n <= 2) return 1; 
+
+        int root = arr.get(0);
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        for (int i = 1; i < n; i++) {
+            if (arr.get(i) < root) left.add(arr.get(i));
+            else right.add(arr.get(i));
+        }
+
+        long leftWays = solve(left, nCr);
+        long rightWays = solve(right, nCr);
+
+        // Choose positions of left or right subtree nodes among total children sirf ek ke liye hi lenge dusre apne aap adjust honge .... sirf ek part ko hi arrange krenge ..
+        long waysToInterleave = nCr[n - 1][left.size()];
+
+        return (((waysToInterleave * leftWays) % MOD) * rightWays) % MOD;
+    }
+
+    public int numOfWays(int[] nums) {
+        int n = nums.length;
+
+        long[][] nCr = new long[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            nCr[i][0] = nCr[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                nCr[i][j] = (nCr[i - 1][j - 1] + nCr[i - 1][j]) % MOD;
+            }
+        }
+
+        List<Integer> arr = new ArrayList<>();
+        for (int x : nums) arr.add(x);
+
+        return (int)((solve(arr, nCr) - 1 + MOD) % MOD);
+    }
+}
