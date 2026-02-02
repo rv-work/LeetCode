39dce016ec711e9f7b83de6fefa4fd451fa657b7
rv-work <rv.dthought@gmail.com>
2026-02-02 23:05:17 +0530

    501

--- Tree/Easy/501.java
+++ Tree/Easy/501.java
@@ -41,3 +41,85 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxFreq = Integer.MIN_VALUE;
+    List<Integer> ans = new ArrayList<>();
+
+    void fill(TreeNode root ,int max,int node){
+        if(root == null){
+            if(max == maxFreq && !ans.contains(node)) ans.add(node);
+            return;
+        } 
+        if(root.val == node){
+            fill(root.left , max+1, node);
+            fill(root.right , max+1, node);
+        }else {
+            if(max == maxFreq &&  !ans.contains(node) ) ans.add(node);
+            fill(root.left , 1, root.val);
+            fill(root.right , 1, root.val);
+        }
+    }
+
+    void func(TreeNode root, int max, int node) {
+        if (root == null) {
+            maxFreq = Math.max(maxFreq, max);
+            return;
+        }
+        if (root.val == node) {
+            func(root.left, max + 1, node);
+            func(root.right, max + 1, node);
+        } else {
+            maxFreq = Math.max(maxFreq, max);
+            func(root.left, 1, root.val);
+            func(root.right, 1, root.val);
+        }
+    }
+
+    public int[] findMode(TreeNode root) {
+        func(root, Integer.MIN_VALUE, Integer.MIN_VALUE);
+        fill(root, Integer.MIN_VALUE, Integer.MIN_VALUE);
+
+        int arr[] = new int[ans.size()];
+        int i = 0;
+        for(int ele : ans){
+            arr[i++] = ele;
+        }
+
+        return arr;
+    }
+}
+
+
+// ye kaam nhi krega kyunki.... ye jaruri nhi ki sare same elements direct relation me ho...
+// like ............
+
+    //         6
+    //      /     \
+    //    2         8
+    //  /   \     /   \
+    // 0     4   7     9
+    //      / \
+    //     2   6
