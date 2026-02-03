5f22150de1047d6e20b682b98777e0c4cab8a154
rv-work <rv.dthought@gmail.com>
2026-02-03 23:03:23 +0530

    260

--- Bit Manipulation/Medium/260.java
+++ Bit Manipulation/Medium/260.java
@@ -27,6 +27,7 @@ class Solution {
 
 
 
+
 class Solution {
     public int[] singleNumber(int[] nums) {
         int xor = 0;
@@ -42,3 +43,51 @@ class Solution {
         return new int[]{a, b};
     }
 }
+
+
+
+
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for (int num : nums) xor ^= num;
+
+        // find rightmost set bit in xor
+        // int k = 0;
+        // for (int i = 0; i < 32; i++) {
+        //     if (((xor >> i) & 1) == 1) {
+        //         k = i;
+        //         break;
+        //     }
+        // }
+
+
+        // int k = xor & -xor;..........alternative
+
+        // eg/..
+        // xor =  10110000
+        // -xor = 01010000 (2’s complement)
+        // -------------------------
+        // &    = 00010000   ← rightmost set bit
+
+
+
+        int a = 0, b = 0; // 2 grps....  set and unset
+        for (int num : nums) {
+            if (((num >> k) & 1) == 1) a ^= num;
+            else b ^= num;
+        }
+
+
+//alternative.../... k be like .. 0010.... so jiska ye vala 1 hoga vo ek me jo nhi hoga to 0 hi aayega vo dusre me 
+        // for (int x : nums) {
+        //    if ((x & diff) == 0) a ^= x;
+        //    else b ^= x;
+        // }
+
+
+        return new int[]{a, b};
+    }
+}
+
