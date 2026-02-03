261e8eade4bbe87d7bf71f814a0df65b11ded1af
rv-work <rv.dthought@gmail.com>
2026-02-03 18:39:13 +0530

    438

--- Strings/Medium/438.java
+++ Strings/Medium/438.java
@@ -41,3 +41,43 @@ class Solution {
 
 
 
+
+
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        if (s.length() < p.length()) return ans;
+
+        int[] freq = new int[26];
+
+        for (char c : p.toCharArray()) freq[c - 'a']++;
+
+        int left = 0, right = 0, count = p.length();
+
+        while (right < s.length()) {
+
+            char rc = s.charAt(right);
+            if (freq[rc - 'a'] > 0) count--;
+            freq[rc - 'a']--;
+            right++;
+
+            if (count == 0) ans.add(left);
+
+            if (right - left == p.length()) {
+                char lc = s.charAt(left);
+                if (freq[lc - 'a'] >= 0) count++;
+                freq[lc - 'a']++;
+                left++;
+            }
+        }
+
+        return ans;
+    }
+}
+
+
+
+
+
+
