f3a7f49d1618ad9dc6402bf34f4e478c29b30937
rv-work <rv.dthought@gmail.com>
2026-02-05 23:42:44 +0530

    1312

--- DP/Hard/1312.java
+++ DP/Hard/1312.java
@@ -54,3 +54,63 @@ class Solution {
         return dp[0][n - 1];
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+
+    int LCS(int n1, int n2, String s1, String s2, int dp[][]) {
+        
+        if (n1 == 0 || n2 == 0)
+            return 0;
+        
+        if(dp[n1-1][n2-1]!=-1) return dp[n1-1][n2-1];
+
+        if (s1.charAt(n1-1) == s2.charAt(n2-1)) {
+            dp[n1-1][n2-1]  =  1 + LCS(n1 - 1, n2 - 1, s1, s2, dp);
+        } else {
+            dp[n1-1][n2-1] = Math.max(LCS(n1 - 1, n2, s1, s2, dp), LCS(n1, n2 - 1, s1, s2, dp));
+        }
+
+        return dp[n1-1][n2-1];
+    }
+
+    public int minInsertions(String s) {
+        int n = s.length();
+
+        StringBuilder sb = new StringBuilder(s);
+
+        String s2 = sb.reverse().toString();
+
+        int dp[][] = new int[n][n];
+        for(int []row:dp) Arrays.fill(row,-1);
+
+        return n - LCS(n, n, s, s2, dp);
+    }
+}
+/*
+
+zza
+zza
+6 - 2*()
+
+mbadm
+mdabm
+
+(10 - 2*(3))/2
+
+
+leetcode
+edocteel
+
+16 - 2*()
+
+*/
