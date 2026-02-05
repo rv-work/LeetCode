48bc5512472f642aa26a0b2d7c41835c5bbe8876
rv-work <rv.dthought@gmail.com>
2026-02-05 23:35:03 +0530

    1312

--- DP/Hard/1312.java
+++ DP/Hard/1312.java
@@ -22,3 +22,35 @@ class Solution {
         return res(0,n-1,s,dp);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        // len is substring length
+        for (int len = 2; len <= n; len++) {       
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+}
