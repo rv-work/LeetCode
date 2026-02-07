3f391df6041cdd8a38c24341dbc7da6ff6c3451e
rv-work <rv.dthought@gmail.com>
2026-02-07 15:09:37 +0530

    POTD : 07-02-2026

--- POTD/Feb/07-02-2026.java
+++ POTD/Feb/07-02-2026.java
@@ -19,3 +19,44 @@ class Solution {
         return cnt;
     }
 }
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int minimumDeletions(String s) {
+        int n = s.length();
+
+        int preB[] = new int[n];
+        int suffA[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'b')
+                preB[i]++;
+            if (i > 0)
+                preB[i] += preB[i - 1];
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == 'a')
+                suffA[i]++;
+            if (i < n - 1)
+                suffA[i] += suffA[i + 1];
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = -1; i < n; i++) {
+            int left = (i >= 0) ? preB[i] : 0;
+            int right = (i + 1 < n) ? suffA[i + 1] : 0;
+            ans = Math.min(ans, left + right);
+        }
+
+        return ans;
+    }
+}
