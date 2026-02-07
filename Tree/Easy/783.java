2028646293ed9d7ac0547c91e9f533fe86f18036
rv-work <rv.dthought@gmail.com>
2026-02-07 15:26:19 +0530

    783

--- Tree/Easy/783.java
+++ Tree/Easy/783.java
@@ -1,3 +1,39 @@
-public class 783 {
-  
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int min = Integer.MAX_VALUE;
+    Integer prev = null;
+
+    public int minDiffInBST(TreeNode root) {
+        inorder(root);
+        return min;
+    }
+
+    void inorder(TreeNode node) {
+        if (node == null) return;
+
+        inorder(node.left);
+
+        // process current node
+        if (prev != null) {
+            min = Math.min(min, node.val - prev);
+        }
+        prev = node.val;
+
+        inorder(node.right);
+    }
 }
