0a2a32b68dadc47a9be866279e731e015c6aa48e
rv-work <rv.dthought@gmail.com>
2026-02-11 22:06:38 +0530

    115

--- DP/Hard/115.java
+++ DP/Hard/115.java
@@ -21,3 +21,42 @@ class Solution {
         return res(s,t,0,0,dp);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        // dp[i][j] = ways to form t[0..j-1] using s[0..i-1]
+        long[][] dp = new long[n + 1][m + 1];
+
+        // empty t can be formed from any prefix of s
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return (int) dp[n][m];
+    }
+}
