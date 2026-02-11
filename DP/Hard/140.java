eb50c0fb03b44573b33c1a2d617ad74cb5a99b82
rv-work <rv.dthought@gmail.com>
2026-02-11 23:15:34 +0530

    140

--- DP/Hard/140.java
+++ DP/Hard/140.java
@@ -50,3 +50,42 @@ class Solution {
 
 
 
+
+
+
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>(wordDict);
+        Map<Integer, List<String>> memo = new HashMap<>();
+        return dfs(0, s, dict, memo);
+    }
+
+    private List<String> dfs(int index, String s, Set<String> dict,
+                             Map<Integer, List<String>> memo) {
+
+        if (memo.containsKey(index)) return memo.get(index);
+
+        List<String> result = new ArrayList<>();
+
+        if (index == s.length()) {
+            result.add(""); // empty string to allow joining
+            return result;
+        }
+
+        for (int j = index + 1; j <= s.length(); j++) {
+            String sub = s.substring(index, j);
+
+            if (dict.contains(sub)) {
+                List<String> rest = dfs(j, s, dict, memo);
+
+                for (String r : rest) {
+                    if (r.isEmpty()) result.add(sub);
+                    else result.add(sub + " " + r);
+                }
+            }
+        }
+
+        memo.put(index, result);
+        return result;
+    }
+}
