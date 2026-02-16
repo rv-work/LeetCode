2558998eff0b3cca0942b0812dced6e5fc8ebbb4
rv-work <rv.dthought@gmail.com>
2026-02-16 12:30:01 +0530

    152

--- Arrays/Medium/152.java
+++ Arrays/Medium/152.java
@@ -23,3 +23,49 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    int res(int l, int r, int[] nums) {
+        if (l == r)
+            return nums[l];
+        int max = Integer.MIN_VALUE;
+
+        int pr = 1;
+        for (int i = l; i <= r; i++) {
+            pr *= nums[i];
+            max = Math.max(max, pr);
+        }
+
+        pr = 1;
+        for (int i = r; i >= l; i--) {
+            pr *= nums[i];
+            max = Math.max(max, pr);
+        }
+
+        return max;
+    }
+
+    public int maxProduct(int[] nums) {
+        List<Integer> zeros = new ArrayList<>();
+        zeros.add(-1);
+        int n = nums.length;
+        if (n == 1)
+            return nums[0];
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0)
+                zeros.add(i);
+        }
+        zeros.add(n);
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < zeros.size() - 1; i++) {
+            max = Math.max(max, res(zeros.get(i) + 1, zeros.get(i + 1) - 1, nums));
+        }
+
+        return zeros.size() > 2 && max < 0 ? 0 : max;
+    }
+}
+
+// split by zeros and then in that zone ..... if even -ve's then multiply all ..
+// if odd then either skip the first or the last whichever gives max...
