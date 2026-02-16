efe18c6cf459f5638a968c5e1651553ab62609a8
rv-work <rv.dthought@gmail.com>
2026-02-16 17:17:59 +0530

    1186

---
+++ Arrays/Medium/1186.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maximumSum(int[] arr) {
+        int n = arr.length;
+        int pre[] = new int[n];
+        int suff[] = new int[n];
+        int max = Integer.MIN_VALUE;
+        int curr = 0;
+        
+        for(int i = 0; i<n; i++){
+            curr += arr[i];
+            if(arr[i] > curr){
+                curr = arr[i];
+            }
+            pre[i] = curr;
+            max = Math.max(max ,curr);
+        }
+
+        curr = 0;
+        for(int i = n-1; i>=0; i--){
+            curr += arr[i];
+            if(arr[i] > curr){
+                curr = arr[i];
+            }
+            suff[i] = curr;
+            max = Math.max(max ,curr);
+        }
+
+        for(int i = 1; i<n-1; i++){
+             max = Math.max(max ,pre[i-1]+suff[i+1]);
+        }
+
+        return max;
+    }
+}
