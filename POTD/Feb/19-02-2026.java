7191d0e1cf65226b7f117521ff59e1b8108fa505
rv-work <rv.dthought@gmail.com>
2026-02-19 07:07:54 +0530

    POTD : 19-02-2026

--- POTD/Feb/19-02-2026.java
+++ POTD/Feb/19-02-2026.java
@@ -32,3 +32,43 @@ class Solution {
         return count;
     }
 }
+
+
+
+
+
+
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int n = s.length();
+        int count = 0;
+
+        // Sirf ek loop chalayenge
+        for (int i = 1; i < n; i++) {
+            
+            // Check: Kya yahan character badal raha hai?
+            // Chahe 0->1 ho ya 1->0, dono chalenge.
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                
+                int left = i - 1;
+                int right = i;
+                
+                // Hame ye yaad rakhna padega ki center par kya tha
+                // Taaki expand karte waqt 'Left' wala wahi rahe aur 'Right' wala wahi rahe
+                char leftChar = s.charAt(left);
+                char rightChar = s.charAt(right);
+
+                // Expand Logic
+                while (left >= 0 && right < n && 
+                       s.charAt(left) == leftChar && s.charAt(right) == rightChar) {
+                    
+                    count++;
+                    left--;
+                    right++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
