2bd1d009c03b3b10c86b3f12ea528745d989765a
rv-work <rv.dthought@gmail.com>
2026-02-19 12:07:38 +0530

    218

--- Heap/Hard/218.java
+++ Heap/Hard/218.java
@@ -214,3 +214,56 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<int[]> points = new ArrayList<>();
+
+        for(int b [] : buildings){
+            points.add(new int[]{b[0] , -b[2]});
+            points.add(new int[]{b[1] , b[2]});
+        }
+
+        Collections.sort(points , (a,b) -> {
+            if(a[0] == b[0]) return a[1]-b[1];
+            return a[0]-b[0];
+        });
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        pq.add(0);
+        int prevMax = 0;
+        for(int p [] : points){
+            int x = p[0];
+            int h = p[1];
+
+            if(h < 0) { // matlab ye start hai to add
+             pq.add(-h);
+            } else {
+                pq.remove(h);
+            }
+
+
+            int currMax = pq.peek();
+
+            if(currMax != prevMax){
+                ans.add(Arrays.asList(x , currMax));
+                prevMax = currMax;
+            }
+        }
+
+
+        return ans;
+    }
+}
