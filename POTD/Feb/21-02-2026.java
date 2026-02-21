5e736f8722a4c4f2ecc53c36c075ccc7554665fa
rv-work <rv.dthought@gmail.com>
2026-02-21 09:46:06 +0530

    POTD : 21-02-2026

--- POTD/Feb/21-02-2026.java
+++ POTD/Feb/21-02-2026.java
@@ -30,3 +30,38 @@ class Solution {
 
    
 }
+
+
+
+
+
+
+
+
+
+class Solution {
+
+    int countOnes(int x) {
+        int cnt = 0;
+        while (x > 0) {
+            cnt += (x & 1);
+            x >>= 1;
+        }
+        return cnt;
+    }
+    public int countPrimeSetBits(int left, int right) {
+
+        // Precomputed small prime set kyunki max 32 bits hain to ye yhi tak ho skta acc to constraints
+        Set<Integer> primes = Set.of(2,3,5,7,11,13,17,19);
+
+        int ans = 0;
+
+        for (int num = left; num <= right; num++) {
+            int bits = countOnes(num);
+
+            if (primes.contains(bits)) ans++;
+        }
+
+        return ans;
+    }
+}
