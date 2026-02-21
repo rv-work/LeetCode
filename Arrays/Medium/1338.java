5adc203a4f1ecb6b9bfb552b29083738db424d63
rv-work <rv.dthought@gmail.com>
2026-02-21 13:05:29 +0530

    1338

--- Arrays/Medium/1338.java
+++ Arrays/Medium/1338.java
@@ -24,3 +24,25 @@ class Solution {
         return count;
     }
 }
+
+
+
+class Solution {
+    public int minSetSize(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : arr) map.put(x, map.getOrDefault(x, 0) + 1);
+
+        PriorityQueue<Integer> pq =
+            new PriorityQueue<>((a, b) -> b - a);
+
+        pq.addAll(map.values());
+
+        int removed = 0, count = 0, half = arr.length / 2;
+        while (removed < half) {
+            removed += pq.poll();
+            count++;
+        }
+
+        return count;
+    }
+}
