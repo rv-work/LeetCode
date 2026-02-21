17f2ae295230bd9fff8a1375f5510a61696abcc1
rv-work <rv.dthought@gmail.com>
2026-02-21 09:47:18 +0530

    POTD : 21-02-2026

--- POTD/Feb/21-02-2026.java
+++ POTD/Feb/21-02-2026.java
@@ -65,3 +65,28 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+
+        boolean[] prime = new boolean[32];
+        prime[2] = prime[3] = prime[5] = prime[7] = prime[11] =
+        prime[13] = prime[17] = prime[19] = prime[23] = prime[29] = prime[31] = true;
+
+        int ans = 0;
+
+        for (int n = left; n <= right; n++) {
+            int bits = Integer.bitCount(n);
+            if (prime[bits]) ans++;
+        }
+
+        return ans;
+    }
+}
