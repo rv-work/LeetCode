68ee79aa01fe030180e78082d8e69eea30223faf
rv-work <rv.dthought@gmail.com>
2026-02-21 12:04:48 +0530

    41

--- Arrays/Hard/41.java
+++ Arrays/Hard/41.java
@@ -19,3 +19,38 @@ class Solution {
         return -1; 
     }
 }
+
+
+
+
+
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        boolean is1 = false;
+
+        for(int i = 0; i<n; i++){
+            if(nums[i] == 1) is1 = true;
+            if(nums[i] <= 0 || nums[i] > n) {
+                nums[i] = 1;
+            }
+        }
+
+        if(!is1) return 1;
+
+        for(int i = 0; i<n; i++){
+            int num = Math.abs(nums[i]);
+            if(nums[num-1] > 0) {   
+                nums[num-1] *= -1;
+            }
+        }
+
+        for(int i = 0; i<n; i++){
+            if(nums[i] > 0) return i+1;
+        }
+
+        return n+1;
+
+    }
+}
