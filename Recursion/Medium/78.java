29f0d462202ac9aac93f19cd590007304c370f6c
rv-work <rv.dthought@gmail.com>
2026-02-22 12:50:04 +0530

    78

--- Recursion/Medium/78.java
+++ Recursion/Medium/78.java
@@ -21,3 +21,30 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+
+        solve(0, nums, temp, ans);
+        return ans;
+    }
+
+    void solve(int idx, int[] nums, List<Integer> temp, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(temp));
+
+        for (int i = idx; i < nums.length; i++) {
+            temp.add(nums[i]);            
+            solve(i + 1, nums, temp, ans); 
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
