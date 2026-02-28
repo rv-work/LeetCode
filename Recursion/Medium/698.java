9214d430b67971a77e7e7a0ba49d2b3b27ee878d
rv-work <rv.dthought@gmail.com>
2026-02-28 07:25:03 +0530

    698

--- Recursion/Medium/698.java
+++ Recursion/Medium/698.java
@@ -15,14 +15,12 @@ class Solution {
                 if (poss) return true;
                 taken[i] = false;
 
-                // --- PRO HACK 1: Empty Bucket Fail ---
                 // Agar bucket khali thi, aur pehla hi number aage chalkar fail ho gaya,
                 // to aage check karne ka koi fayda nahi, seedha return false!
                 if (target == bucket) {
                     return false;
                 }
 
-                // --- PRO HACK 2: Duplicate Skip ---
                 // Agar current number fail ho gaya, aur agla number bhi bilkul same hai,
                 // to wo bhi fail hi hoga. Use skip kar do! (Kyunki array sorted hai)
                 while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
@@ -38,7 +36,6 @@ class Solution {
         int sum = Arrays.stream(nums).sum();
         if (sum % k != 0) return false;
         
-        // --- PRO HACK 3: Reverse Sorting ---
         Arrays.sort(nums);
         for (int i = 0, j = nums.length - 1; i < j; i++, j--) {
             int temp = nums[i];
