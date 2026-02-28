cff8c5e4b147624a6c736a7d8f6b490e74830069
rv-work <rv.dthought@gmail.com>
2026-02-28 08:11:05 +0530

    698

--- Recursion/Medium/698.java
+++ Recursion/Medium/698.java
@@ -48,3 +48,79 @@ class Solution {
         return res(0, nums, k, sum / k, sum / k, taken);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    
+
+    boolean res(int index, int[] nums, int k, int target, int bucket, int mask, Boolean[] dp) {
+        
+        if (k == 1) return true; 
+
+        if (target == 0) {
+            // Nayi bucket shuru, index wapas 0
+            return res(0, nums, k - 1, bucket, bucket, mask, dp);
+        }
+
+        if (dp[mask] != null) {
+            return dp[mask];
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            
+            // !taken[i] ko bitmask se check kiya: 
+            // (1 << i) ka matlab hai 'i' th position par 1 baaki sab 0. 
+            // AND (&) karne par 0 aane ka matlab hai wo bit abhi off (0) hai.
+            if ((mask & (1 << i)) == 0 && nums[i] <= target) {
+                
+                // Naya mask banaya: OR (|) operator bit ko ON (1) kar deta hai
+                int newMask = mask | (1 << i);
+                
+                boolean poss = res(i + 1, nums, k, target - nums[i], bucket, newMask, dp);
+                if (poss) {
+                    return dp[mask] = true; // State save karke return true
+                }
+
+                // Empty bucket fail
+                if (target == bucket) {
+                    break; // break the loop, aage check mat karo
+                }
+
+                //  Skip duplicates
+                while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+                    i++;
+                }
+            }
+        }
+
+        // Agar koi bhi combination kaam nahi aaya, to is mask state ko false save kar do
+        return dp[mask] = false;
+    }
+
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = Arrays.stream(nums).sum();
+        if (sum % k != 0) return false;
+        
+        Arrays.sort(nums);
+        for (int i = 0, j = nums.length - 1; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        int n = nums.length;
+  
+        Boolean[] dp = new Boolean[1 << n];
+        
+        return res(0, nums, k, sum / k, sum / k, 0, dp);
+    }
+}
