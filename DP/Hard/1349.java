14bfd24a0eba21d887ecd8ae8c60828b633bf7a3
rv-work <rv.dthought@gmail.com>
2026-02-28 08:54:32 +0530

    1349

--- DP/Hard/1349.java
+++ DP/Hard/1349.java
@@ -43,3 +43,64 @@ class Solution {
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    int res(int[] brokenRows, int[][] dp, int row, int prevMask, int m) {
+        int n = dp.length;
+        if (row == n) return 0;
+
+        if (dp[row][prevMask] != -1) return dp[row][prevMask];
+
+        int maxStudents = 0; 
+
+        for (int currMask = 0; currMask < (1 << m); currMask++) {
+
+            if ((currMask & (currMask >> 1)) != 0) continue;
+            
+            if ((currMask & brokenRows[row]) != 0) continue;
+            
+            if ((currMask & (prevMask >> 1)) != 0) continue;
+            if ((currMask & (prevMask << 1)) != 0) continue;
+
+            int cnt = Integer.bitCount(currMask);
+            
+            int total = cnt + res(brokenRows, dp, row + 1, currMask, m);
+            
+            maxStudents = Math.max(maxStudents, total);
+        }
+
+        return dp[row][prevMask] = maxStudents;
+    }
+
+    public int maxStudents(char[][] seats) {
+        int n = seats.length;    
+        int m = seats[0].length; 
+        
+        int[][] dp = new int[n][1 << m];
+        for (int[] row : dp) Arrays.fill(row, -1);
+
+        int[] brokenRows = new int[n];
+        
+        for (int r = 0; r < n; r++) {
+            int brokenMask = 0;
+            for (int c = 0; c < m; c++) {
+                if (seats[r][c] == '#') {
+                    brokenMask |= (1 << c);
+                }
+            }
+            brokenRows[r] = brokenMask;
+        }
+        
+        return res(brokenRows, dp, 0, 0, m);
+    }
+}
