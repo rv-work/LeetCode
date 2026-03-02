f0e8e6dcc446bf4f99c47e7a0d8f1c9e1120fb8f
rv-work <rv.dthought@gmail.com>
2026-03-02 20:41:13 +0530

    274

--- Arrays/Medium/274.java
+++ Arrays/Medium/274.java
@@ -54,3 +54,21 @@ class Solution {
         return 0;
     }
 }
+
+
+
+
+
+class Solution {
+    public int hIndex(int[] c) {
+        Arrays.sort(c); 
+        int n = c.length;
+        for (int i = 0; i < n; i++) {
+            int h = n - i;      
+            if (c[i] >= h) {
+                return h;
+            }
+        }
+        return 0;
+    }
+}
