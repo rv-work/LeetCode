7f8fc573c9aca9341e171f712d0fdfdc62818de5
rv-work <rv.dthought@gmail.com>
2026-03-02 20:03:42 +0530

    274

---
+++ Arrays/Medium/274.java
@@ -0,0 +1,30 @@
+class Solution {
+    boolean check(int cnt , int arr[]){
+        int lim = cnt;
+
+        for(int num : arr){
+            if(num >= lim){
+                cnt--;
+                if(cnt == 0) return true;
+            }
+        }
+
+        return false;
+    }
+    public int hIndex(int[] c) {
+        int n = c.length;
+        int low = 1; int high = n;
+        int ans = 0;
+        while(low <= high){
+            int mid = (low+high)>>1;
+            if(check(mid , c)){
+                ans = mid;
+                low = mid+1;
+            }else {
+                high = mid-1;
+            }
+        }
+
+        return ans;
+    }
+}
