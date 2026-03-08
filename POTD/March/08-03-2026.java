fede08eb69041b6ee5e6579ef4459a5a07bfefe0
rv-work <rv.dthought@gmail.com>
2026-03-08 07:55:52 +0530

    POTD : 08-03-2026

--- POTD/March/08-03-2026.java
+++ POTD/March/08-03-2026.java
@@ -68,3 +68,20 @@ class Solution {
     return solve(curr + "1", n, set);
   }
 }
+
+class Solution {
+  public String findDifferentBinaryString(String[] nums) {
+
+    int n = nums.length;
+    StringBuilder ans = new StringBuilder();
+
+    for (int i = 0; i < n; i++) {
+      if (nums[i].charAt(i) == '0')
+        ans.append('1');
+      else
+        ans.append('0');
+    }
+
+    return ans.toString();
+  }
+}
