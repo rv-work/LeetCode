b0be5150b9775a50b6fddc74ff163cfd6c3c82a4
rv-work <rv.dthought@gmail.com>
2026-03-10 21:40:45 +0530

    POTD : 10-03-2026

---
+++ POTD/March/10-03-2026.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        int MOD = 1000000007;
+        
+        // dp[i][j][0] -> valid arrays with 'i' zeros, 'j' ones, ending with '0'
+        // dp[i][j][1] -> valid arrays with 'i' zeros, 'j' ones, ending with '1'
+        long[][][] dp = new long[zero + 1][one + 1][2];
+
+        // Base Cases Initialization
+        // Agar sirf 0s use kar rahe hain (limit tak):
+        for (int i = 1; i <= Math.min(zero, limit); i++) {
+            dp[i][0][0] = 1;
+        }
+        // Agar sirf 1s use kar rahe hain (limit tak):
+        for (int j = 1; j <= Math.min(one, limit); j++) {
+            dp[0][j][1] = 1;
+        }
+
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                
+                // 1. Calculate for ending with '0'
+                // Naya element add kiya (previous valid state)
+                dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
+                
+                // Jo element limit se bahar (window ke peeche) chhut gaya, usko subtract karo
+                if (i > limit) {
+                    dp[i][j][0] = (dp[i][j][0] - dp[i - limit - 1][j][1] + MOD) % MOD;
+                }
+
+                // 2. Calculate for ending with '1'
+                // Naya element add kiya
+                dp[i][j][1] = (dp[i][j - 1][1] + dp[i][j - 1][0]) % MOD;
+                
+                // Jo element limit se bahar chhut gaya, usko subtract karo
+                if (j > limit) {
+                    dp[i][j][1] = (dp[i][j][1] - dp[i][j - limit - 1][0] + MOD) % MOD;
+                }
+            }
+        }
+
+        return (int) ((dp[zero][one][0] + dp[zero][one][1]) % MOD);
+    }
+}
