17b84ee0d53b74b38db5f3a693c6ef3a1737af2d
rv-work <rv.dthought@gmail.com>
2026-03-13 06:55:54 +0530

    POTD : 13-03-2026

---
+++ POTD/March/13-03-2026.java
@@ -0,0 +1,64 @@
+class Solution {
+
+    boolean check(int mh, int[] wt, long mid) {
+
+        for (int w : wt) {
+            long x = (long) ((Math.sqrt(1.0 + 8.0 * mid / w) - 1.0) / 2.0);
+            mh -= x;
+            if (mh <= 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // boolean check(int mh, int[] wt, long mid) {
+    //     long totalHeightReduced = 0;
+
+    //     for (int w : wt) {
+    //         long low = 1, high = mh, canBreak = 0;
+
+    //         while (low <= high) {
+    //             long h = low + (high - low) / 2;
+    //             long timeNeeded = (long) w * h * (h + 1) / 2;
+
+    //             if (timeNeeded <= mid) {
+    //                 canBreak = h;
+    //                 low = h + 1;
+    //             } else {
+    //                 high = h - 1;
+    //             }
+    //         }
+
+    //         totalHeightReduced += canBreak;
+    //         if (totalHeightReduced >= mh)
+    //             return true;
+    //     }
+    //     return false;
+    // }
+
+    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+        long maxWorkerTime = 0;
+        for (int time : workerTimes) {
+            maxWorkerTime = Math.max(maxWorkerTime, time);
+        }
+
+        long H = mountainHeight;
+        long high = maxWorkerTime * (H * (H + 1)) / 2;
+        long low = 1;
+
+        long ans = -1;
+
+        while (low <= high) {
+            long mid = low + ((high - low) >> 1);
+            if (check(mountainHeight, workerTimes, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+
+        return ans;
+    }
+}
