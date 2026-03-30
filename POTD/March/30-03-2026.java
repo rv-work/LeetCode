aa807db46bc72a873086f5280f9ab3600839808e
rv-work <rv.dthought@gmail.com>
2026-03-30 18:09:49 +0530

    POTD : 30-03-2026

--- POTD/March/30-03-2026.java
+++ POTD/March/30-03-2026.java
@@ -1,6 +1,36 @@
-for (int i = 0; i < even.length; i++) {
-    int num1 = even[i];
-    int num2 = odd[i];
+class Solution {
+  public boolean checkStrings(String s1, String s2) {
+    int n = s1.length();
 
-    if (num1 != 0 || num2 != 0) return false;
+    int even[] = new int[26];
+    int odd[] = new int[26];
+
+    for (int i = 0; i < n; i++) {
+      char c = s1.charAt(i);
+      if (i % 2 == 0) {
+        even[c - 'a']++;
+      } else {
+        odd[c - 'a']++;
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      char c = s2.charAt(i);
+      if (i % 2 == 0) {
+        even[c - 'a']--;
+      } else {
+        odd[c - 'a']--;
+      }
+    }
+
+    for (int i = 0; i < even.length; i++) {
+      int num1 = even[i];
+      int num2 = odd[i];
+
+      if (num1 != 0 || num2 != 0)
+        return false;
+    }
+
+    return true;
+  }
 }
